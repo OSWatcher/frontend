7da18f5dff214d422157f72f2cbb7430e4483951
Mathieu Tarral <mathieu.tarral@protonmail.com>
2020-03-21 22:27:48 +0100

    os_view: display root filesystem

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,33 @@
-from flask import render_template
+from pathlib import PurePath
 
-from oswatcher.model import OS
+from flask import render_template
+from oswatcher.model import OS, GraphInode
 
 from . import app
+
+GRAPH = app.config['GRAPH']
 
 
 @app.route('/')
 def home_page():
-    graph = app.config['GRAPH']
-    os_list = [os.name for os in OS.match(graph)]
+    os_list = [os.name for os in OS.match(GRAPH)]
     return render_template('home.html', os_list=os_list)
 
 
 @app.route('/os/<os_name>')
-def os_view(os_name):
-    return render_template('os_view.html', os_name=os_name)
+@app.route('/os/<os_name>/<path:fs_path>')
+def os_view(os_name, fs_path=None):
+    if fs_path is None:
+        fs_path = PurePath('/')
+    else:
+        fs_path = PurePath(fs_path)
+
+    cypher_query = "MATCH (:OS {{name: '{os_name}'}})-[:OWNS_FILESYSTEM]->(root:GraphInode {{name: '/' }})".format(
+        os_name=os_name)
+    for part in fs_path.parts[1:]:
+        subquery = "-[:HAS_CHILD]->(:GraphInode {{name: '{folder_name}'}})".format(folder_name=part)
+        cypher_query += subquery
+    cypher_query += "-[:HAS_CHILD]->(child:GraphInode) RETURN child"
+    print(cypher_query)
+    cursor = GRAPH.run(cypher_query)
+    return render_template('os_view.html', os_name=os_name, fs_path=fs_path, cursor=cursor)
