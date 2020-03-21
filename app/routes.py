5ddc190423395ed8493a263dac7ae65de9f64edc
Mathieu Tarral <mathieu.tarral@protonmail.com>
2020-03-21 20:58:28 +0100

    list os in home page

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,17 @@
 from flask import render_template
 
+from oswatcher.model import OS
+
 from . import app
 
 
 @app.route('/')
 def home_page():
-    return render_template('home.html')
+    graph = app.config['GRAPH']
+    os_list = [os.name for os in OS.match(graph)]
+    return render_template('home.html', os_list=os_list)
+
+
+@app.route('/os/<os_name>')
+def os_view(os_name):
+    return render_template('os_view.html', os_name=os_name)
