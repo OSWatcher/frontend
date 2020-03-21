680af51003366e71e8dd02d007f9166f7f62b6ab
Mathieu Tarral <mathieu.tarral@protonmail.com>
2020-03-21 20:58:28 +0100

    list os in home page

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,24 @@
+from pathlib import Path
+
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from py2neo import Graph
+
+APP_DIR = Path(__file__).parent.parent
 
 # Define the WSGI application object
 app = Flask(__name__)
 
 # Apply Bootstrap
 Bootstrap(app)
+
+# Load configuration
+app.config.from_pyfile(APP_DIR / 'config.cfg')
+
+# init neo4j graph object
+graph = Graph(password=app.config['DB_PASSWORD'])
+# make py2neo graph available for Flask app
+app.config['GRAPH'] = graph
 
 # define routes
 from . import routes
