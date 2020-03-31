7d8b00158aed815c031d9730d358701db991afc6
Mathieu Tarral <mathieu.tarral@protonmail.com>
2020-03-31 20:15:48 +0200

    frontend: add navbar

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-navbar type="dark" variant="dark">
+    <b-navbar-brand href="#">OSWatcher</b-navbar-brand>
+  </b-navbar>
+</template>
+
+<style scoped>
+.navbar {
+    padding: .5rem 1rem;
+    margin-bottom: 2rem;
+}
+
+.navbar-brand {
+    padding-left: 3rem;
+    padding-right: 3rem
+}
+</style>
