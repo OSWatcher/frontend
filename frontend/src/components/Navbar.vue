d4cbc29d90932c4753a8273a3e7536808ce369ea
Mathieu Tarral <mathieu.tarral@protonmail.com>
2020-03-31 21:43:52 +0200

    frontend: add logo with navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,17 +1,31 @@
 <template>
-  <b-navbar type="dark" variant="dark">
-    <b-navbar-brand href="#">OSWatcher</b-navbar-brand>
+  <b-navbar type="dark">
+    <b-navbar-brand href="#">
+      <img src="@/assets/logo.png" alt="logo" />
+    </b-navbar-brand>
+    <b-navbar-nav>
+      <b-nav-item to="/">Home</b-nav-item>
+    </b-navbar-nav>
   </b-navbar>
 </template>
 
 <style scoped>
 .navbar {
-    padding: .5rem 1rem;
-    margin-bottom: 2rem;
+  padding: 0;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  margin-bottom: 2rem;
+  height: 4rem;
+  background-color: #3b4a6b;
 }
 
 .navbar-brand {
-    padding-left: 3rem;
-    padding-right: 3rem
+  padding: 0;
+  padding-left: 1.5rem;
+  padding-right: 1.5rem;
+}
+
+.navbar-brand img {
+  width: 13.3rem;
 }
 </style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-      <h1>Home page !</h1>
+    <h1>Home page !</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home",
+  name: "Home"
 };
 </script>
