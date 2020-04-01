cd8d45865ec059e6ac3a3f759e6a2a45f6526be2
Mathieu Tarral <mathieu.tarral@protonmail.com>
2020-04-01 20:49:35 +0200

    frontend: display fake os list in homepage

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,11 +1,33 @@
 <template>
   <div>
-    <h1>Home page !</h1>
+    <h2 id="title">Operating Systems</h2>
+    <b-list-group>
+      <b-list-group-item to="/" v-for="os in osList" :key="os">
+        {{ os }}
+      </b-list-group-item>
+    </b-list-group>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home"
+  name: "Home",
+  data () {
+    return {
+      osList: [
+        "Windows XP",
+        "Windows Vista",
+        "Windows 7",
+        "Windows 8",
+        "Windows 10"
+      ]
+    };
+  }
 };
 </script>
+
+<style scoped>
+#title {
+  padding-bottom: 2rem;
+}
+</style>
