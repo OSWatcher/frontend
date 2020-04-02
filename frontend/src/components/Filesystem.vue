667c57dab75c526e462c9e864cbefbefee994390
Mathieu Tarral <mathieu.tarral@protonmail.com>
2020-04-02 02:50:02 +0200

    frontend: display root filesystem in OSView

--- frontend/src/components/Filesystem.vue
+++ frontend/src/components/Filesystem.vue
@@ -1,10 +1,55 @@
 <template>
   <div>
+    <h2>Filesystem</h2>
+    <b-list-group>
+      <b-list-group-item
+        v-for="entry in fs_entries"
+        :key="entry.name"
+      >
+        {{ entry.name }}
+      </b-list-group-item>
+    </b-list-group>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "Filesystem"
+  name: "Filesystem",
+  props: {
+    name: {
+      required: true,
+      type: String
+    },
+  },
+  data() {
+    return {
+      fs_path: "/",
+      fs_entries: []
+    };
+  },
+  methods: {
+    get_message() {
+      const path = `http://localhost:5000/list_fs_at?os_name=${this.name}&fs_path=${this.fs_entries}`;
+      axios.get(path)
+        .then((res) => {
+          this.fs_entries = res.data.fs_entries;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  },
+  created() {
+    this.get_message();
+  }
 };
 </script>
+
+<style scoped>
+.list-group {
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+}
+</style>
