011b2e052ca53396e88e332d004d89baba31389a
Mathieu Tarral <mathieu.tarral@protonmail.com>
2020-04-02 02:32:33 +0200

    frontend: add empty Filesystem component in OSView

--- frontend/src/views/OSView.vue
+++ frontend/src/views/OSView.vue
@@ -3,12 +3,18 @@
     <h2 id="title">
       {{name}}
     </h2>
+    <Filesystem />
   </div>
 </template>
 
 <script>
+import Filesystem from "@/components/Filesystem.vue";
+
 export default {
   name: "OSView",
+  components: {
+    Filesystem
+  },
   props: {
     name: {
       required: true,
