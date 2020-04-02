9195253907025e9041b761fe3241ee095497fcba
Mathieu Tarral <mathieu.tarral@protonmail.com>
2020-04-03 00:49:19 +0200

    frontend: add click event on folder and sort entries

--- frontend/src/components/Filesystem.vue
+++ frontend/src/components/Filesystem.vue
@@ -4,12 +4,20 @@
     <b-breadcrumb :items="fs_path_parts"></b-breadcrumb>
     <b-list-group>
       <b-list-group-item
-        v-for="entry in fs_entries"
+        v-for="entry in fs_folder_entries"
+        :key="entry.name"
+        :action="true"
+        v-on:click="on_item_clicked"
+      >
+        <b-icon icon="folder-fill"></b-icon>
+
+        {{ entry.name }}
+      </b-list-group-item>
+      <b-list-group-item
+        v-for="entry in fs_file_entries"
         :key="entry.name"
       >
-        
-        <b-icon v-if="entry.inode_type == 16384" icon="folder-fill"></b-icon>
-        <b-icon v-else icon="file-earmark"></b-icon>
+        <b-icon icon="file-earmark"></b-icon>
         {{ entry.name }}
       </b-list-group-item>
     </b-list-group>
@@ -31,19 +39,32 @@ export default {
     return {
       fs_path: "/",
       fs_path_parts: ["Root"],
-      fs_entries: []
+      fs_folder_entries: [],
+      fs_file_entries: []
     };
   },
   methods: {
     get_message() {
-      const path = `http://localhost:5000/list_fs_at?os_name=${this.name}&fs_path=${this.fs_entries}`;
+      const path = `http://localhost:5000/list_fs_at?os_name=${this.name}&fs_path=${this.fs_path}`;
       axios.get(path)
         .then((res) => {
-          this.fs_entries = res.data.fs_entries;
+          this.fs_folder_entries = res.data.fs_entries.filter(entry => this.inode_is_dir(entry.inode_type)).sort(function(a, b) {
+            return a.name > b.name;
+          });
+          this.fs_file_entries = res.data.fs_entries.filter(entry => !this.inode_is_dir(entry.inode_type)).sort(function(a, b) {
+            return a.name > b.name;
+          });
         })
         .catch((error) => {
           console.error(error);
         });
+    },
+    inode_is_dir(inode_type) {
+      return inode_type == 16384 ? true : false;
+    },
+    on_item_clicked (event) {
+      // check if dir
+      console.log(event);
     }
   },
   created() {
