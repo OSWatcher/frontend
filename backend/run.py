9be181cc83b4cd5bd773ac3295dcdd200f22c92d
Mathieu Tarral <mathieu.tarral@protonmail.com>
2020-04-12 23:55:47 +0200

    use logging instead of print statements

--- backend/run.py
+++ backend/run.py
@@ -8,14 +8,21 @@ Options:
     -d --debug                      Enable debug output
 """
 
+import logging
 # local
 from app import app
 # 3rd
 from docopt import docopt
 
 
+LOG_FORMAT = "%(asctime)s %(levelname)s:%(name)s:%(message)s"
+
 def main():
     args = docopt(__doc__)
+    level = logging.INFO
+    if args['--debug']:
+        level = logging.DEBUG
+    logging.basicConfig(level=level, format=LOG_FORMAT)
     app.run(debug=args['--debug'])
 
 
