b125a524a823a99f717b6ad7e55e7097dc0e9cc7
Mathieu Tarral <mathieu.tarral@protonmail.com>
2020-04-12 23:57:49 +0200

    logging: silence neobolt output

--- backend/run.py
+++ backend/run.py
@@ -17,12 +17,18 @@ from docopt import docopt
 
 LOG_FORMAT = "%(asctime)s %(levelname)s:%(name)s:%(message)s"
 
+
 def main():
     args = docopt(__doc__)
     level = logging.INFO
     if args['--debug']:
         level = logging.DEBUG
     logging.basicConfig(level=level, format=LOG_FORMAT)
+    # suppress py2neo protocol output
+    logging.getLogger("httpstream").setLevel(logging.WARNING)
+    logging.getLogger("neo4j.bolt").setLevel(logging.WARNING)
+    logging.getLogger("neobolt").setLevel(logging.WARNING)
+    # run server
     app.run(debug=args['--debug'])
 
 
