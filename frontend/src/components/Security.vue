0c0b4a8cc99e8996484999da75a4d5fcb56836fe
Mathieu Tarral <mathieu.tarral@protonmail.com>
2020-04-15 19:25:46 +0200

    frontend: display checksec table

--- frontend/src/components/Security.vue
+++ frontend/src/components/Security.vue
@@ -1,18 +1,42 @@
 <template>
   <div>
-    <!-- Setuid binaries (if Linux) -->
     <template v-if="os.type == 'Linux'">
+      <!-- Setuid binaries -->
       <h2>
         Setuid Binaries
         <b-badge variant="secondary">{{ setuid_count }}</b-badge>
       </h2>
-      <b-overlay :show=is_loading>
+      <b-overlay :show=is_loading class="mb-5">
         <b-list-group>
           <b-list-group-item v-for="item in setuid_list" :key="item.path" class="text-monospace">
             {{ item.path }}
           </b-list-group-item>
         </b-list-group>
       </b-overlay>
+      <!-- Checksec table -->
+      <h2>
+        Checksec Binaries
+        <b-badge variant="secondary">{{ checksec_count }}</b-badge>
+      </h2>
+      <b-pagination
+        v-model="checksec_current_page"
+        :total-rows="checksec_count"
+        :per-page="checksec_per_page"
+        aria-controls="checksec_table"
+      ></b-pagination>
+      <b-table
+        id="checksec_table"
+        striped
+        hover
+        bordered
+        :items="checksec_items_list"
+        :fields="checksec_fields"
+        :per-page="checksec_per_page" 
+        :current-page="checksec_current_page"
+        :busy="is_loading"
+        sort-by="path"
+      >
+      </b-table>
     </template>
   </div>
 </template>
@@ -32,7 +56,54 @@ export default {
   data() {
     return {
       is_loading: true,
-      setuid_list: []
+      // linux
+      setuid_list: [],
+      checksec_items_list: [],
+      checksec_fields: [
+        {
+          key: 'path',
+          sortable: true,
+          tdClass: 'text-monospace'
+        },
+        {
+          key: 'name',
+          sortable: true,
+          tdClass: 'text-monospace'
+        },
+        {
+          key: 'relro',
+          label: 'Relocation Read-Only',
+          sortable: true,
+        },
+        {
+          key: 'canary',
+          sortable: true,
+        },
+        {
+          key: 'nx',
+          label: 'Non-Executable',
+          sortable: true,
+        },
+        {
+          key: 'pie',
+          label: 'Position-Independant Executable',
+          sortable: true,
+        },
+        {
+          key: 'rpath',
+          sortable: true,
+        },
+        {
+          key: 'runpath',
+          sortable: true,
+        },
+        {
+          key: 'symbols',
+          sortable: true,
+        },
+      ],
+      checksec_per_page: 10,
+      checksec_current_page: 1
     };
   },
   methods: {
@@ -48,10 +119,25 @@ export default {
           console.error(error);
         });
     },
+    list_checksec() {
+      const url = `http://localhost:5000/os/${this.os.id}/filesystem/search`;
+      const params = {'checksec': true};
+
+      axios.post(url, params)
+        .then((res) => {
+          this.checksec_items_list = res.data.result;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
   },
   computed: {
     setuid_count () {
       return this.setuid_list.length;
+    },
+    checksec_count () {
+      return this.checksec_items_list.length;
     }
   },
   created() {
@@ -76,7 +162,10 @@ export default {
       this.is_loading = false;  
       return Promise.reject(error);
     });
-    this.list_setuid();
+    if (this.os.type == 'Linux') {
+      this.list_setuid();
+      this.list_checksec();
+    }
   }
 };
 </script>
@@ -84,6 +173,11 @@ export default {
 <style scoped>
 .list-group {
   height: 30rem;
+  overflow: auto;
+}
+
+.table {
+  height: 10rem;
   overflow: auto;
 }
 </style>
