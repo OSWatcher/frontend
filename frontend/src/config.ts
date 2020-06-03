b39b80dc39028642ca2453bc1d3c452115f7f80d
Mathieu Tarral <mathieu.tarral@protonmail.com>
2020-06-03 22:15:14 +0200

    frontend: switch to flask adhoc certificates

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,5 @@
 module.exports = {
   API_LOCATION: process.env.NODE_ENV === 'production'
-    ? 'http://34.65.83.178:5000'
-    : 'http://localhost:5000'
+    ? 'https://34.65.83.178:5000'
+    : 'https://localhost:5000'
 };
