7081e353d46e3e28410353be057e65f5c529257b
Mathieu Tarral <mathieu.tarral@protonmail.com>
2024-05-05 17:46:13 +0200

    osview: display os title

--- src/views/OSView.vue
+++ src/views/OSView.vue
@@ -5,11 +5,12 @@ import FilesystemTree from '@/components/FilesystemTree.vue'
 import RegistryTree from '@/components/RegistryTree.vue'
 import PDBExplorer from '@/components/PDBExplorer.vue'
 import gqlClient from '@/graphql-client'
-import { BTabs, BTab, BSpinner } from 'bootstrap-vue-next'
+import { BTabs, BTab, BSpinner, BCard, BRow, BCol } from 'bootstrap-vue-next'
 import { getCommitCapabilities } from '@/queries'
 
 const route = useRoute()
 const os_hash = route.params.os_hash
+const os_title = route.query.os_title
 // v-if isn't supported, we need to build the tabs variable and insert entries instead
 const tabs = reactive({
   filesystem: { title: 'Filesystem', component: markRaw(FilesystemTree) }
@@ -41,7 +42,14 @@ onMounted(async () => {
 
 <template>
   <div class="container">
-    <BSpinner v-if="isLoading"></BSpinner>
+    <BRow>
+      <BCol cols="4">
+        <BCard :title="os_title" class="mb-3 p-3"></BCard>
+      </BCol>
+      <BCol>
+        <BSpinner v-if="isLoading"></BSpinner>
+      </BCol>
+    </BRow>
     <b-tabs content-class="mt-3">
       <b-tab v-for="(tab, key) in tabs" :key="key" :title="tab.title">
         <component :is="tab.component" :os_hash="os_hash" />
