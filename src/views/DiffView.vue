c6c021628977d89778dba2e535d5c86f1f4f0e71
Mathieu Tarral <mathieu.tarral@protonmail.com>
2024-06-08 02:10:46 +0200

    view: add DiffView

---
+++ src/views/DiffView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+import { BCard } from 'bootstrap-vue-next'
+
+interface Commit {
+  hash: string
+  name: string
+}
+
+// get route params
+const route = useRoute()
+const base_commit = {
+  hash: route.params.base_hash,
+  name: route.query.base_name
+}
+const diffee_commit = {
+  hash: route.params.diffee_hash,
+  name: route.query.diffee_name
+}
+</script>
+
+<template>
+  <BCard title="Diff View">
+    <p>Base Commit: {{ base_commit.name }}</p>
+    <p>Diffee Commit: {{ diffee_commit.name }}</p>
+  </BCard>
+</template>
