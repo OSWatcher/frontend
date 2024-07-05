4d76f88a86c847e1ba5081c6a2c5142707785b23
Mathieu Tarral <mathieu.tarral@protonmail.com>
2024-07-05 04:19:17 +0200

    components: extract out CommitsTable.vue

--- src/types.ts
+++ src/types.ts
@@ -3,4 +3,16 @@ enum TreeNodeType {
   Tree
 }
 
+interface Commit {
+  hash: string
+  name: string
+  date: string
+  selected?: boolean
+}
+
+interface BranchesWithCommits {
+  [key: string]: Commit[]
+}
+
 export default TreeNodeType
+export type { Commit, BranchesWithCommits }
