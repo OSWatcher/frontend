f4d6954cd8d04d8fe0e13be453d8d9ceef04fcb2
Mathieu Tarral <mathieu.tarral@protonmail.com>
2024-09-10 20:34:29 +0200

    types: add HashDiff interface

--- src/types.ts
+++ src/types.ts
@@ -11,6 +11,12 @@ interface Commit {
   selected?: boolean
 }
 
+// simple type for diff hashes
+export interface HashDiff {
+  base_hash: string | null
+  diffee_hash: string | null
+}
+
 interface BranchesWithCommits {
   [key: string]: Commit[]
 }
