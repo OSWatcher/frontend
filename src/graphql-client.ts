210aae951d1cf7a02b64944379a969026122a459
Mathieu Tarral <mathieu.tarral@protonmail.com>
2024-10-14 15:12:03 +0200

    graphql-client: use TypedTypePolicies

--- src/graphql-client.ts
+++ src/graphql-client.ts
@@ -1,5 +1,6 @@
 import { ApolloClient, InMemoryCache, HttpLink, from } from '@apollo/client/core'
 import { onError } from '@apollo/client/link/error'
+import { TypedTypePolicies } from '@/graphql-types'
 
 // Define the API URL through an environment variable
 const httpLink = new HttpLink({
@@ -15,10 +16,29 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (networkError) console.log(`[Network error]: ${networkError}. Backend is unreachable.`)
 })
 
+// Define type policies
+const typePolicies: TypedTypePolicies = {
+  // Add your type policies here, for example:
+  // Query: {
+  //   fields: {
+  //     someField: {
+  //       merge(existing, incoming) {
+  //         // Custom merge function
+  //       }
+  //     }
+  //   }
+  // }
+}
+
 // Apollo Client instance
 const gqlClient = new ApolloClient({
   link: from([errorLink, httpLink]), // Link chain: error handling followed by HTTP connection
-  cache: new InMemoryCache()
+  cache: new InMemoryCache({ typePolicies }),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-and-network',
+    },
+  },
 })
 
 export default gqlClient
