a384b045861c30dd6e0dd82688043dcbd1985f46
Mathieu Tarral <mathieu.tarral@protonmail.com>
2024-10-15 09:39:04 +0200

    PDBExplorerDiff: refactor with hashdiff and generated queries

--- src/components/diff/PDBExplorerDiff.vue
+++ src/components/diff/PDBExplorerDiff.vue
@@ -4,74 +4,59 @@ clicked from the filesystem view By default, it should explore the commit hash f
 to locate /Windows/System32/ntoskrnl.exe and display its symbols.
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, PropType } from 'vue'
 import gqlClient from '@/graphql-client'
-import { TRAVERSE_PATH, GET_FS_ROOT } from '@/queries'
 import { BTabs, BTab, BCard, BRow, BCol } from 'bootstrap-vue-next'
 import SymbolDiffView from '@/components/diff/pdb/SymbolDiffView.vue'
+import { HashDiff } from '@/types'
+import { fetchFSRootCommitDiff } from '@/utils'
+import {
+  TraversePathQuery,
+  TraversePathQueryVariables,
+  TraversePathDocument
+} from '@/graphql-types'
 
 const NTOSKRNL_PATH = '/Windows/System32/ntoskrnl.exe'
 
 const props = defineProps({
-  base_commit: {
-    type: String,
-    required: true
-  },
-  diffee_commit: {
-    type: String,
+  commitHashDiff: {
+    type: Object as PropType<HashDiff>,
     required: true
   }
 })
 
-const base_commit = ref({
-  hash: props.base_commit,
-  fs_root_hash: undefined,
-  ntos_hash: undefined
-})
-const diffee_commit = ref({
-  hash: props.diffee_commit,
-  fs_root_hash: undefined,
-  ntos_hash: undefined
-})
+const ntos_hashdiff = ref<HashDiff | null>(null)
 
 const blob_name = ref('ntoskrnl.exe')
-const blob_hash = ref(null)
 
 onMounted(async () => {
   try {
-    let response = await gqlClient.query({
-      query: GET_FS_ROOT,
-      variables: { where: { hash_IN: [base_commit.value.hash, diffee_commit.value.hash] } }
-    })
-    // assign fs_root_hash to the corresponding commit
-    response.data['commits'].forEach((commit) => {
-      if (commit.hash === base_commit.value.hash) {
-        base_commit.value.fs_root_hash = commit.filesystemConnection.edges[0].node.hash
-      } else if (commit.hash === diffee_commit.value.hash) {
-        diffee_commit.value.fs_root_hash = commit.filesystemConnection.edges[0].node.hash
-      }
-    })
-
+    const fs_root_diff = await fetchFSRootCommitDiff(props.commitHashDiff)
     // Traverse fs root to locate /Windows/System32/ntoskrnl.exe for both commits
-    const baseNtosResponse = await gqlClient.query({
-      query: TRAVERSE_PATH,
-      variables: {
-        parent_label: 'Tree',
-        tree_hash: base_commit.value.fs_root_hash,
-        path: NTOSKRNL_PATH
-      }
-    })
-    const diffeeNtosResponse = await gqlClient.query({
-      query: TRAVERSE_PATH,
-      variables: {
-        parent_label: 'Tree',
-        tree_hash: diffee_commit.value.fs_root_hash,
-        path: NTOSKRNL_PATH
-      }
-    })
+    const [baseNtosResponse, diffeeNtosResponse] = await Promise.all([
+      gqlClient.query<TraversePathQuery, TraversePathQueryVariables>({
+        query: TraversePathDocument,
+        variables: {
+          parent_label: 'Tree',
+          tree_hash: fs_root_diff.base_hash!,
+          path: NTOSKRNL_PATH
+        }
+      }),
+      gqlClient.query<TraversePathQuery, TraversePathQueryVariables>({
+        query: TraversePathDocument,
+        variables: {
+          parent_label: 'Tree',
+          tree_hash: fs_root_diff.diffee_hash!,
+          path: NTOSKRNL_PATH
+        }
+      })
+    ])
 
-    base_commit.value.ntos_hash = baseNtosResponse.data['traversePath']
-    diffee_commit.value.ntos_hash = diffeeNtosResponse.data['traversePath']
+    ntos_hashdiff.value = {
+      base_hash: baseNtosResponse.data.traversePath!,
+      diffee_hash: diffeeNtosResponse.data.traversePath!,
+      label: 'Blob'
+    }
   } catch (error) {
     console.error('Error fetching commit details', error)
   }
@@ -85,12 +70,9 @@ onMounted(async () => {
         <BCard :title="blob_name"></BCard>
       </BCol>
     </BRow>
-    <BTabs content-class="mt-3" v-if="base_commit.ntos_hash && diffee_commit.ntos_hash">
+    <BTabs content-class="mt-3" v-if="ntos_hashdiff">
       <BTab title="Symbols">
-        <SymbolDiffView
-          :base_blob_hash="base_commit.ntos_hash"
-          :diffee_blob_hash="diffee_commit.ntos_hash"
-        />
+        <SymbolDiffView :blob_hash_diff="ntos_hashdiff" />
       </BTab>
     </BTabs>
   </div>
