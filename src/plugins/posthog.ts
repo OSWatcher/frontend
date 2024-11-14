71d5d12459266b1157e23e9056830070a6a8bb7b
Mathieu Tarral <mathieu.tarral@protonmail.com>
2024-11-14 01:30:56 +0100

    posthog: proxied to api/events

--- src/plugins/posthog.ts
+++ src/plugins/posthog.ts
@@ -4,7 +4,7 @@ import type { App } from 'vue'
 export default {
   install: (_app: App) => {
     posthog.init('REDACTED_POSTHOG_KEY', {
-      api_host: 'https://us.i.posthog.com' // or your self-hosted URL
+      api_host: new URL('events', import.meta.env.VITE_GRAPHEOS_API_URI).toString()
     })
   }
 }
