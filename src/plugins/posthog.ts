079ad3d30ce2d2959e6bdaf7ce704af0ecb30921
Mathieu Tarral <mathieu.tarral@protonmail.com>
2025-08-24 15:54:24 +0200

    posthogs: fix fmt

--- src/plugins/posthog.ts
+++ src/plugins/posthog.ts
@@ -5,14 +5,14 @@ export default {
   install: (_app: App) => {
     const apiKey = import.meta.env.VITE_POSTHOG_API_KEY
     const apiUri = import.meta.env.VITE_GRAPHEOS_API_URI
-    
+
     if (!apiKey) {
       throw new Error('VITE_POSTHOG_API_KEY environment variable is not defined')
     }
     if (!apiUri) {
       throw new Error('VITE_GRAPHEOS_API_URI environment variable is not defined')
     }
-    
+
     posthog.init(apiKey, {
       api_host: new URL('events', apiUri).toString()
     })
