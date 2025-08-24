eacd4f7e5b14f7a2ad9d11037c88cf62a469ba6d
Mathieu Tarral <mathieu.tarral@protonmail.com>
2025-08-24 17:09:44 +0200

    main: provide Apollo client to the app

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,14 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import App from './App.vue'
 import router from './router'
 import posthogPlugin from './plugins/posthog'
+import { provideApolloClient } from '@vue/apollo-composable'
+import gqlClient from './graphql-client'
 
 const app = createApp(App)
+
+// Provide Apollo client to the entire app
+provideApolloClient(gqlClient)
+
 app.use(createBootstrap())
 app.use(router)
 
